5a1a7ab310fe9a90249a478b5f6d6ff52a0fa88d
Kristen Shea <kstar61@live.com>
2018-08-01 18:14:09 -0500

    started

--- CG 4-3 (raffle)/Program.cs
+++ CG 4-3 (raffle)/Program.cs
@@ -6,7 +6,37 @@ namespace CG_4_3__raffle_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Raffle");
+
+           
+            Console.Write($"Enter name 1:  ");
+            string nameOne = Console.ReadLine();
+
+            Console.Write($"Enter name 2: ");
+            string nameTwo = Console.ReadLine();
+
+
+
+            /*foreach (var item in collection)
+            {
+
+            }*/
+
+            
+
+            //gather up to 30 (can be less than 30) names from user. they will hit enter without name if done
+            //randomly choose a name from array and print as winner
+            //print list of loser names
+
+
+
+
+
+
+            //Console.WriteLine($"The Raffle Winner is: {winner});
+            //Console.ReadLine($"Raffle losers: ");
+            Console.ReadLine();
+
         }
     }
 }

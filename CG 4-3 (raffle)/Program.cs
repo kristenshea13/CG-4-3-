22455a0636ed61f53258b21453a09f9f386c48f8
Kristen Shea <kstar61@live.com>
2018-08-01 17:41:17 -0500

    Add project files.

---
+++ CG 4-3 (raffle)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_4_3__raffle_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

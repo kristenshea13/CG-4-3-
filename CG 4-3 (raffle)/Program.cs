f089f3d6bb3d074d05137ee65e7d8f3db259f374
Kristen Shea <kstar61@live.com>
2018-08-02 00:03:53 -0500

    raffle

--- CG 4-3 (raffle)/Program.cs
+++ CG 4-3 (raffle)/Program.cs
@@ -15,12 +15,12 @@ namespace CG_4_3__raffle_
             //declare array
             string[] userNames = new string[30];
             int number = 0;
-            //declare number as an int so it can be used to represent number of name in array
+            //declare number as an int so it can be used to represent numbers of names in array
 
 
 
             for (int i = 0; i < 30; i++)
-                //less than 30 is used because you want 30 names or less, and numbering starts on 0 so it will end on 29
+                //30 names or less, use 0 through 29
             {
                 //clarify for user what number the name they are entering is. since number starts at 0 must add one so user's first entry is #1
                 Console.Write("Name {0}: ", i + 1);
@@ -31,7 +31,7 @@ namespace CG_4_3__raffle_
 
 
                 //add *break* to break out of loop if user enters blank entry
-                //use if statement, if null or white space, found in intellisense, because they are finished making entries 
+                //use if statement and if null or white space because they are finished making entries 
                 if (string.IsNullOrWhiteSpace(raffleInfo)) break;
                 number = i;
                 //this variable was declared as an integer above, needed to be assigned the value of the number of each name
@@ -43,17 +43,18 @@ namespace CG_4_3__raffle_
             var rnd = new Random();
             var winnerName = rnd.Next(number);
             Console.WriteLine($"The raffle winner is: { userNames[winnerName]}!");
-            //print winner name from list of user input names
+            //print winner name from randomized choice from inside array
 
             Console.WriteLine("Sorry, you lose: ");
 
             //create another loop to print losers names from corresponding numbers and exclude printing winner name
 
             for (int L = 0; L <= number; L++)
+                //i had to use less than or equal to number, less than number was cutting off the last value, adding = seems to have fixed the issue
             {
                 if (L == winnerName)
                     continue;
-                //if statement using *continue*, to skip over winner name and continue printing the rest of the names to console
+                //if statement using *continue* to skip over winner name and continue printing the rest of the names to console
                 Console.WriteLine(userNames[L]);
             }
 

04aee051db5f0e9e416187b9a0b6396f84ea90ac
Kristen Shea <kstar61@live.com>
2018-08-01 23:45:48 -0500

    raffle

--- CG 4-3 (raffle)/Program.cs
+++ CG 4-3 (raffle)/Program.cs
@@ -7,69 +7,58 @@ namespace CG_4_3__raffle_
         static void Main(string[] args)
         {
 
-            /*Create a program that asks the user to enter up to 30 names.If the user hits enter 
-             * without entering a name, assume they are done and stop asking them.  Then you will 
-             * randomly choose a name from the array and write it to the console as the winner.We haven't 
-             * learned how to randomize yet, so you'll need to stretch your Googling skills.Then you will write 
-             * the list of other names that did not win.*/
 
             Console.WriteLine("Raffle");
-
-            //ask number of names after gathering names
-
-
-
-
            
-            Console.Write($"Enter up to 30 names:  ");
-            string nameOne = Console.ReadLine();
+            Console.WriteLine($"Enter up to 30 names, hit enter key after each.  ");
+            
 
             //declare array
-            string[] names = new string[30];
+            string[] userNames = new string[30];
+            int number = 0;
+            //declare number as an int so it can be used to represent number of name in array
 
-            
-        
 
-
-           //loop 
-           //random
-           //break out of loop with enter
-           //how to keep null coming up with random
-           //change 30 to smaller number for testing
 
             for (int i = 0; i < 30; i++)
+                //less than 30 is used because you want 30 names or less, and numbering starts on 0 so it will end on 29
             {
-                names[i] = 
-                Console.ReadLine();
+                //clarify for user what number the name they are entering is. since number starts at 0 must add one so user's first entry is #1
+                Console.Write("Name {0}: ", i + 1);
+
+                string raffleInfo = Console.ReadLine();
+                userNames[i] = raffleInfo;
+                //connect array to user input names and corresponding numbers for inputs.
+
+
+                //add *break* to break out of loop if user enters blank entry
+                //use if statement, if null or white space, found in intellisense, because they are finished making entries 
+                if (string.IsNullOrWhiteSpace(raffleInfo)) break;
+                number = i;
+                //this variable was declared as an integer above, needed to be assigned the value of the number of each name
+
             }
 
 
-            foreach (var item in names)
-            {
-                Console.Write(item);
-                
+            //to randomize user input names to get winner name
+            var rnd = new Random();
+            var winnerName = rnd.Next(number);
+            Console.WriteLine($"The raffle winner is: { userNames[winnerName]}!");
+            //print winner name from list of user input names
 
-                
+            Console.WriteLine("Sorry, you lose: ");
+
+            //create another loop to print losers names from corresponding numbers and exclude printing winner name
+
+            for (int L = 0; L <= number; L++)
+            {
+                if (L == winnerName)
+                    continue;
+                //if statement using *continue*, to skip over winner name and continue printing the rest of the names to console
+                Console.WriteLine(userNames[L]);
             }
-
-            /*foreach (var item in collection)
-            {
-
-            }*/
 
             
-
-            //gather up to 30 (can be less than 30) names from user. they will hit enter without name if done
-            //randomly choose a name from array and print as winner
-            //print list of loser names
-
-
-
-
-
-
-            //Console.WriteLine($"The Raffle Winner is: {winner});
-            //Console.ReadLine($"Raffle losers: ");
             Console.ReadLine();
 
         }

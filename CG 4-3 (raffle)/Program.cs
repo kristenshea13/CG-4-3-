7cb6da3e28b2b0e9bcfdbe2ba0e344870c2abe4b
Kristen Shea <kstar61@live.com>
2018-08-01 19:56:27 -0500

    declared array and started loop

--- CG 4-3 (raffle)/Program.cs
+++ CG 4-3 (raffle)/Program.cs
@@ -6,17 +6,51 @@ namespace CG_4_3__raffle_
     {
         static void Main(string[] args)
         {
+
+            /*Create a program that asks the user to enter up to 30 names.If the user hits enter 
+             * without entering a name, assume they are done and stop asking them.  Then you will 
+             * randomly choose a name from the array and write it to the console as the winner.We haven't 
+             * learned how to randomize yet, so you'll need to stretch your Googling skills.Then you will write 
+             * the list of other names that did not win.*/
+
             Console.WriteLine("Raffle");
 
-            //ask number of names
+            //ask number of names after gathering names
 
-            Console.Write($"Enter name 1:  ");
+
+
+
+           
+            Console.Write($"Enter up to 30 names:  ");
             string nameOne = Console.ReadLine();
 
-            Console.Write($"Enter name 2: ");
-            string nameTwo = Console.ReadLine();
+            //declare array
+            string[] names = new string[30];
+
+            
+        
 
 
+           //loop 
+           //random
+           //break out of loop with enter
+           //how to keep null coming up with random
+           //change 30 to smaller number for testing
+
+            for (int i = 0; i < 30; i++)
+            {
+                names[i] = 
+                Console.ReadLine();
+            }
+
+
+            foreach (var item in names)
+            {
+                Console.Write(item);
+                
+
+                
+            }
 
             /*foreach (var item in collection)
             {
